66291bcf4d20f81112fd30a94980856899e455cd
jerome11sgic <jerome11sgic@gmail.com>
2020-03-18 23:56:31 +0530

    test

---
+++ build/precache-manifest.a6915e235ddddf93cebe77e1b053d6ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828fee00f5214365a528ca7434fef7cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08d50ffd7d29c1fdae0b",
+    "url": "/static/css/main.8fea470d.chunk.css"
+  },
+  {
+    "revision": "1bd24fb4bcc626e2c6cb",
+    "url": "/static/js/2.9ce18ce9.chunk.js"
+  },
+  {
+    "revision": "a0ea6e3ddf93c63bc17c407bf65f7628",
+    "url": "/static/js/2.9ce18ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d50ffd7d29c1fdae0b",
+    "url": "/static/js/main.4139db87.chunk.js"
+  },
+  {
+    "revision": "6320eb2ace6de74dae28",
+    "url": "/static/js/runtime-main.443d2dae.js"
+  }
+]);
